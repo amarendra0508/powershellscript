ef3bcd9a5e572678789adc56e622be06e51ee79d
amarendra0508 <amarendra.kumar@live.in>
2017-01-19 22:50:23 +0530

    script updated 10:50pm

--- iis.ps1
+++ iis.ps1
@@ -1,1 +1,17 @@
-xcopy /s C:\REPO C:\BKP\BKP
+
+# --------------------------------------------------------------------
+# Define the variables.
+# --------------------------------------------------------------------
+$InetPubRoot = "C:\Inetpub"
+$InetPubLog = "C:\Inetpub\Log"
+$InetPubWWWRoot = "C:\Inetpub\WWWRoot"
+
+# --------------------------------------------------------------------
+# Loading Feature Installation Modules
+# --------------------------------------------------------------------
+Import-Module ServerManager 
+
+# --------------------------------------------------------------------
+# Installing IIS
+# --------------------------------------------------------------------
+Add-WindowsFeature -Name Web-Common-Http,Web-Asp-Net,Web-Net-Ext,Web-ISAPI-Ext,Web-ISAPI-Filter,Web-Http-Logging,Web-Request-Monitor,Web-Basic-Auth,Web-Windows-Auth,Web-Filtering,Web-Performance,Web-Mgmt-Console,Web-Mgmt-Compat,WAS -IncludeAllSubFeature
